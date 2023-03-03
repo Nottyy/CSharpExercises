9893d7440a6ae9f56704d4a71ae610826092ca23
Antonio <antonio.marinovvvv@gmail.com>
2023-03-03 13:48:44 +0200

    dp - findMinNum - recursion

---
+++ DP - FindMinNumberRecursion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP___FindMinNumberRecursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 4, 9, -2, 0, 22, 111, 3, 5 };
+            Console.WriteLine(FindMinNum(arr, arr.Length));
+        }
+
+        private static int FindMinNum(int[] arr, int length)
+        {
+            if (length == 1)
+            {
+                return arr[0];
+            }
+
+            var minTillNow = FindMinNum(arr, length - 1);
+
+            return minTillNow < arr[length - 1] ? minTillNow : arr[length - 1];
+        }
+    }
+}
