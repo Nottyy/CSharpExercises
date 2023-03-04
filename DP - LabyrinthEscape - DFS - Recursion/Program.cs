92a291a3140d208fbd4bd522e547b05f0ca15ac1
Antonio <antonio.marinovvvv@gmail.com>
2023-03-04 23:54:46 +0200

    count number of possible exits in labyrinth

--- DP - LabyrinthEscape - DFS - Recursion/Program.cs
+++ DP - LabyrinthEscape - DFS - Recursion/Program.cs
@@ -13,13 +13,36 @@ namespace DP___LabyrinthEscape___DFS___Recursion
             var labyrinth = new string[]
             {
                 "     ",
-                " @@@@",
-                "  @@ ",
-                "   @ ",
-                "   @ ",
+                " @@@ ",
+                " @@@ ",
+                "  @  ",
+                "     ",
             };
 
             Console.WriteLine(DfsLabyrinthEscape(labyrinth, 0, 0));
+
+            Console.WriteLine(DfsLabyrinthEscapeCount(labyrinth, 0, 0));
+        }
+
+        private static int DfsLabyrinthEscapeCount(string[] arr, int row, int col)
+        {
+            if (row >= arr.Length || col >= arr[row].Length)
+            {
+                return 0;
+            }
+
+            if (row >= arr.Length - 1 && col >= arr[arr.Length - 1].Length - 1)
+            {
+                Console.WriteLine("Found a way out!");
+                return 1;
+            }
+
+            if (arr[row][col] == '@')
+            {
+                return 0;
+            }
+
+            return DfsLabyrinthEscapeCount(arr, row, col + 1) + DfsLabyrinthEscapeCount(arr, row + 1, col);
         }
 
         private static bool DfsLabyrinthEscape(string[] arr, int row, int col)
@@ -40,8 +63,10 @@ namespace DP___LabyrinthEscape___DFS___Recursion
                 return false;
             }
 
-            return DfsLabyrinthEscape(arr, row, col + 1) ^
-            DfsLabyrinthEscape(arr, row + 1, col);
+            var right = DfsLabyrinthEscape(arr, row, col + 1);
+            var down = DfsLabyrinthEscape(arr, row + 1, col);
+
+            return right || down;
         }
     }
 }
